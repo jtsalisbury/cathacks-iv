b56a81edc2ca2719f8c660d5b82b3d5a400577b3
Kurt Lewis <kurt.jlewis15@gmail.com>
2018-04-07 14:52:09 -0400

    Initialization of sequelize

---
+++ config/config.js
@@ -0,0 +1,30 @@
+const dotenv = require('dotenv').config();
+
+/**
+ * I'm keeping this as an extra step between environment variables and loading 
+ * them, because it could later allow for further configuration of database
+ * connections, which could be useful in development or testing.
+ */
+module.exports = {
+  development: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASS,
+    database: process.env.MYSQL_DB,
+    host: process.env.MYSQL_HOST,
+    dialect: "mysql"
+  },
+  test: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASS,
+    database: process.env.MYSQL_DB,
+    host: process.env.MYSQL_HOST,
+    dialect: "mysql"
+  },
+  production: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASS,
+    database: process.env.MYSQL_DB,
+    host: process.env.MYSQL_HOST,
+    dialect: "mysql"
+  }
+}
