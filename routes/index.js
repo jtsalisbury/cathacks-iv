f14d22d6c7a1183605198db050033832b7c626e2
Zachary Sang <zacharysang@gmail.com>
2018-04-07 22:44:53 +0000

    Added uploads

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,26 @@
 let express = require('express');
 let router = express.Router();
+let formidable = require('formidable');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {title: 'Data Viewer Thingy'});
 });
 
-router.post('/upload', function(req, res, next) {
-  res.render('upload', {title: 'Upload'});
+router.post('/upload', (req, res, next) => {
+  // take in the form and save it
+  let form = new formidable.IncomingForm();
+  form.uploadDir = 'uploads';
+  form.type = true;
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+        res.render('index',
+                    {title: 'Data Viewer Thingy',
+                    errMsg: 'File Upload Failed'});
+      } else {
+        res.render('viz', {title: 'Viz'});
+      }
+  });
 });
 
 router.post('/viz', function(req, res, next) {
