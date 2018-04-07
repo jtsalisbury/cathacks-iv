dec799045351aa89906066afd756cdc3d5a5302e
Kurt Lewis <kurt.jlewis15@gmail.com>
2018-04-07 15:55:56 -0400

    add csvdata and trend models

---
+++ models/csvdata.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var CsvData = sequelize.define('CsvData', {
+    path: DataTypes.STRING,
+    title: DataTypes.STRING,
+    start_date: DataTypes.DATE,
+    end_date: DataTypes.DATE,
+    interval: DataTypes.INTEGER,
+    x_col: DataTypes.INTEGER
+  }, {});
+  CsvData.associate = function(models) {
+    // associations can be defined here
+  };
+  return CsvData;
+};
---
+++ models/trend.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Trend = sequelize.define('Trend', {
+    title: DataTypes.STRING,
+    y_col: DataTypes.INTEGER,
+    trendline_coef: DataTypes.FLOAT
+  }, {});
+  Trend.associate = function(models) {
+    // associations can be defined here
+    models.Trend.belongsTo(models.CsvData)
+  };
+  return Trend;
+};
